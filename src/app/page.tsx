20eeb9932f27abf29f7b587be73bc6b69af725cd
claxespinosa <c.eliza.hernandez@gmail.com>
2026-09-10 23:25:08 -0600

    next dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,23 @@ import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
 import Evolution from "@/components/sections/Evolution";
 import Enfoque from "@/components/sections/Enfoque";
+import dynamic from 'next/dynamic'
+
+// El componente solo se descargará en un chunk separado cuando sea necesario
+const ComponentePesado = dynamic(() => import('../components/ComponentePesado'), {
+  loading: () => <p>Cargando...</p>,
+  ssr: false, // Opcional: desactiva el renderizado en el servidor si usa APIs del navegador (window)
+})
+
+export default function Pagina() {
+  return (
+    <div>
+      <h1>Mi Página</h1>
+      <ComponentePesado />
+    </div>
+  )
+}
+
 
 export default function Home() {
   return (
